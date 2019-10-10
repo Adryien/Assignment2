c7cf2f8a09def083e7a670a3c3f089b929ce4277
adryi <adryi@DESKTOP-M03JJOD>
2019-10-09 20:35:46 -0700

    Initial check in of AddingMachine

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,41 @@
+package cse360assign2;
+//Name: Adryien Hayes
+//CSE 360 Assignment #2
+//Class ID: RB1
+//Description: This program defines the class AddingMachine,
+//which stores a total that can that can be added to or subtracted
+//from to produce a new total.
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//The method getTotal() returns the current total of the AddingMachine
+	public int getTotal () {
+		return 0;
+	}
+	
+	//The method add(int value) adds value to the current total and updates total to the result
+	public void add (int value) {
+		
+	}
+	
+	//The method subtract(int value) subtracts value from the current total and updates the total to the result
+	public void subtract (int value) {
+		
+	}
+	
+	//The toString() method prints out the current total as a String
+	public String toString () {
+		return "";
+	}
+	
+	//clear() resets the total of the AddingMachine to 0.
+	public void clear() {
+	
+	}
+}
